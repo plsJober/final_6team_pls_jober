c0456514b5c9d0f807d81b13de1b48d6018a31c8
이홍영 <hyhy7012@naver.com>
2026-01-18 19:26:12 +0900

    refactor : account정보를 직렬화 하도록 수정

--- back/src/main/java/com/example/config/RedisConfig.java
+++ back/src/main/java/com/example/config/RedisConfig.java
@@ -1,14 +1,19 @@
 package com.example.config;
 
+import com.example.dto.AccountCacheDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
+    /**
+     * String 타입 데이터를 위한 RedisTemplate (기존 - TokenService 등에서 사용)
+     */
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
@@ -19,6 +24,22 @@ public class RedisConfig {
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
+        
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, AccountCacheDto> accountCacheRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, AccountCacheDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        
+        // Key는 String으로 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        // Value는 JSON으로 직렬화 (Account 객체 저장)
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         
         template.afterPropertiesSet();
         return template;
